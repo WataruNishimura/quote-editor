2867f1476dc1c5f15a5a6f3b37c9de7ccfb36deb
Wataru Nishimura <nishimura@mikan-foundation.org>
2025-11-16 21:05:58 +0900

    impl turbo_frame templates

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,9 +15,11 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
-      # Add `status: :unprocessable_entity` here
       render :new, status: :unprocessable_entity
     end
   end
@@ -36,7 +38,11 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private
